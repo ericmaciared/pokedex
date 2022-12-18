e6c791dee8692abc79b554d69380e004e2bfcfac
Javier <javier.merida@mykukun.com>
2022-12-18 15:37:56 +0100

    Moving grid pokemon to another widget class

---
+++ lib/pokemon/widgets/pokemon_grid.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'pokemon_sprite.dart';
+import 'package:pokedex/styles.dart';
+
+class PokemonGrid extends StatelessWidget {
+  Map<String, dynamic>? sprites;
+  int count = 150;
+
+
+  PokemonGrid({super.key, required this.sprites, this.count=150});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 5),
+        padding: EdgeInsets.symmetric(horizontal: Styles.sidePadding),
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: count,
+        itemBuilder: (BuildContext context, int index) {
+          return PokemonSprite(data: sprites!["pokemon_v2_pokemonsprites"][index]["sprites"]);
+        });
+  }
+}
