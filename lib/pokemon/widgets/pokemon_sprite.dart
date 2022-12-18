578588eea695013ea76334eaa8b24b65b67a3a68
Javier <javier.merida@mykukun.com>
2022-12-18 15:38:43 +0100

    Managing the sprites json within the PokemonSprite widget for separation of concerns

--- lib/pokemon/widgets/pokemon_sprite.dart
+++ lib/pokemon/widgets/pokemon_sprite.dart
@@ -1,17 +1,26 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 // Widget to load an image from url and displays it with a specific size.
 class PokemonSprite extends StatelessWidget {
-  String? url;
+  String? data;
   double size = 30;
 
-  PokemonSprite ({super.key, required this.url, this.size=30});
+  PokemonSprite ({super.key, required this.data, this.size=30});
+
+  String getSprite() {
+    final Map<String, dynamic> spritesJson =
+    jsonDecode(data!);
+
+    return spritesJson["front_default"];
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
         height: size,
-        child: Image.network(url!, errorBuilder:
+        child: Image.network(getSprite(), errorBuilder:
             (BuildContext context, Object exception,
             StackTrace? stackTrace) {
           return const Text('ð¢');
