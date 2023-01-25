945dd154a376dc7a0aa50dece2bb451d62112f48
Javier <javier.merida@mykukun.com>
2023-01-25 02:02:08 +0100

    Adding Firebase init app and changing main to Future (as required by firebase app)

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'graphql.dart';
 import 'home.dart';
 import 'login.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const PokedexApp());
 }
 
